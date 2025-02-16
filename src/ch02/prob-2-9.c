2e2efb628291d168c43b293527ad78bbd631329e
fanlumaster <1730976608@qq.com>
2025-02-16 14:19:25 +0800

    temp prob2-9 and 2-10

--- src/ch02/prob-2-9.c
+++ src/ch02/prob-2-9.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    
-    return 0;
+/* bitcount: count 1 bits in x - faster version */
+int bitcount(unsigned x) {
+    int b;
+
+    for (b = 0; x != 0; x &= x - 1)
+        ++b;
+    return b;
 }
+
+int main(int argc, char *argv[]) { return 0; }

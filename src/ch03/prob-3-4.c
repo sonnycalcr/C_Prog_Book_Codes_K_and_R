1c5b48c1580c9ec87a1750440be19f104f2f811a
fanlumaster <1730976608@qq.com>
2025-02-22 02:43:06 +0800

    update

---
+++ src/ch03/prob-3-4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+#define abs(x) ((x) < 0 ? -(x) : (x))
+
+/* reverse: reverse string s in place */
+void reverse(char s[])
+
+{
+    int c, i, j;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* itoa: convert n to characters in s - modified */
+void itoa(int n, char s[]) {
+    int i, sign;
+    sign = n; /* record sign */
+    i = 0;
+
+    do {                            /* generate digits in reverse order */
+        s[i++] = abs(n % 10) + '0'; /* get next digit */
+    } while ((n /= 10) != 0); /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+int main() {
+    char s[100];
+    int n = INT_MIN; // Largest negative number
+
+    itoa(n, s);
+    printf("Integer: %d\nString: %s\n", n, s);
+
+    return 0;
+}

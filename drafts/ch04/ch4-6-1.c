c32b7f5d2da16fa1822dfeac27e0d448b72d1751
fanlumaster <1730976608@qq.com>
2025-02-22 20:45:20 +0800

    an example for static used in function

---
+++ drafts/ch04/ch4-6-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void counter() {
+    static int count = 0; // only initialized when this funciton is called for the first time
+    count++;
+    printf("This function has been called %d times.\n", count);
+}
+
+int main() {
+    counter(); // This function has been called 1 times.
+    counter(); // This function has been called 2 times.
+    counter(); // This function has been called 3 times.
+    return 0;
+}
